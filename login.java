34145322a1cb4e21da0070d27335f4a5aed8f697
Thej <s.thejdeep@gmail.com>
2025-11-06 16:19:40 +0000

    Added java code

---
+++ login.java
@@ -0,0 +1,27 @@
+public class SimpleVariables {
+
+    public static void main(String[] args) {
+        // Declaring and initializing an integer variable
+        int age = 30;
+
+        // Declaring and initializing a string variable
+        String name = "Alice";
+
+        // Declaring and initializing a double variable
+        double height = 1.75;
+
+        // Declaring a boolean variable and assigning a value later
+        boolean isStudent;
+        isStudent = true;
+
+        // Printing the values of the variables
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " meters");
+        System.out.println("Is student: " + isStudent);
+
+        // Modifying the value of a variable
+        age = 31;
+        System.out.println("New age: " + age);
+    }
+}
